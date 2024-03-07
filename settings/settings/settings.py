3f92e337b893c7ad9e642a3dcf065b9300e257bf
DarthZiel <Studerzhan@gmail.com>
2024-03-07 17:26:29 +0500

    login and logout

--- settings/settings/settings.py
+++ settings/settings/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
         },
     },
 ]
-
+AUTH_USER_MODEL = 'players.User'
 WSGI_APPLICATION = "settings.wsgi.application"
 
 
@@ -122,3 +122,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGOUT_REDIRECT_URL = '/'
